a157b7884d8a71090ff56141bce49dfd09eae645
Alam <sonualam1599@gmail.com>
2023-10-03 17:13:45 +0530

    refactor

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,14 @@ const connectDatabase = require("./config/db")
 dotenv.config({ path: "backend/config/config.env" })
 
 
+
 //connecting to database
 connectDatabase()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening to ${process.env.PORT}`)
+app.get("/", (req, res) => {
+    res.json("Connected To Backend")
+})
+
+app.listen(4000, () => {
+    console.log(`Server is listening to ${4000}`)
 })
